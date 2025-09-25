13a8842b81068e8feca28c51408058f4ec5e7259
ch7oe <chloenixon723@gmail.com>
2025-09-25 12:10:29 -0500

    added posts table to database

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa 
 import sqlalchemy.orm as so
 from app import db
+
 
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -9,5 +11,19 @@ class User(db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author") # WriteOnlyMapped defines posts as a collection type of Post objects inside 
+
     def __repr__(self):
         return "<User {}>".format(self.username)
+    
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc)) # index configured to efficiently retrieve posts in chronological order 
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+
+    author: so.Mapped[User] = so.relationship(back_populates="posts")
+
+    def __repr__(self):
+        return "<Post {}>".format(self.body)
