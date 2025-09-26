d4aa8ff93ec9d03164825e983165d42f742c3009
ch7oe <chloenixon723@gmail.com>
2025-09-26 14:04:21 -0500

    completed view function to login a user with flask-login

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
-from app import app
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import app, db
+from app.models import User
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
@@ -23,8 +26,17 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    
     form = LoginForm()
+
     if form.validate_on_submit():
-        flash("Login requested for user {}, remember_me{}".format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+        if user is not None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for("index"))
     return render_template("login.html", title="Sign In", form=form)
