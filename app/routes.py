3f5ce3ed321eee429e168c65fca54b6f9e039f9d
ch7oe <chloenixon723@gmail.com>
2025-09-26 17:52:50 -0500

    added user registration form and page for user registration

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ import sqlalchemy as sa
 from app import app, db
 from app.models import User
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from urllib.parse import urlsplit
 
@@ -54,3 +54,20 @@ def logout():
     """Logout user."""
     logout_user()
     return redirect(url_for("index"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register a new user."""
+
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations, you are now a registered user!")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
