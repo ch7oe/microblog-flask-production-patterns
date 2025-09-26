16a6733cf5b90274f7f1a5b0b841078ce15c3abe
ch7oe <chloenixon723@gmail.com>
2025-09-26 13:21:24 -0500

    added user password hashing and verification

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from typing import Optional
 import sqlalchemy as sa 
 import sqlalchemy.orm as so
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -12,6 +13,12 @@ class User(db.Model):
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
     posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author") # WriteOnlyMapped defines posts as a collection type of Post objects inside 
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return "<User {}>".format(self.username)
