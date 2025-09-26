fff9d6a7f51c6ef94ac0fd4087bc78d9fa4164b2
ch7oe <chloenixon723@gmail.com>
2025-09-26 13:37:06 -0500

    prepared user model for flask-login

--- app/models.py
+++ app/models.py
@@ -2,11 +2,17 @@ from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa 
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    """Return a user by id."""
+    return db.session.get(User, int(id))
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
