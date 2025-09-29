f81edf44e7bf9d2564589866d86e1037d2b77c63
ch7oe <chloenixon723@gmail.com>
2025-09-29 13:35:47 -0500

    added form for users to edit profile about themselves. user profile displays about me section and last visit time for user

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 import sqlalchemy as sa
 from app import db
 from app.models import User
@@ -30,4 +30,10 @@ class RegistrationForm(FlaskForm):
         user = db.session.scalar(sa.selec(User).where(User.email == email.data))
         if user is not None:
             raise ValidationError("Please use a different email address.")
-    
+        
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Username", validator=[DataRequired()])
+    about_me = TextAreaField("About Me", validators=[Length(min=0, max=140)])
+    submit = SubmitField("Submit")
+        
