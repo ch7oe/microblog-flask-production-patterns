2e4b3185e62c2e15953ee5c584d8eb95df69f285
ch7oe <chloenixon723@gmail.com>
2026-02-17 10:23:45 -0600

    flask-mail initialization. email sending wrapper function

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,6 +16,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login" # let Flask-Login know which view function handles logins
+mail = Mail(app)
+
 
 if not app.debug: 
     if app.config["MAIL_SERVER"]:
