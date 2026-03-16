f8703de40d6ec00d281b3d1b83290dc2c5421f3c
ch7oe <chloenixon723@gmail.com>
2026-03-16 15:18:48 -0400

    save language for new posts. add a translation link to posts. update requirements after installing requests package.

--- app/routes.py
+++ app/routes.py
@@ -16,6 +16,7 @@ from app.forms import (
     ResetPasswordForm,
 )
 from app.email import send_password_reset_email
+from langdetect import detect, LangDetectException
 
 
 @app.before_request
@@ -32,7 +33,13 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+
+        try:
+            language = detect(form.post.data)
+        except LangDetectException:
+            language = ''
+        
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash(_("You've posted successfully!"))
