fc1a1c522fcfdb3afce832297157b0cd0a9e90c6
ch7oe <chloenixon723@gmail.com>
2026-03-18 15:17:39 -0500

    Text translation view function (AJAX from server).
    Client-side translation function in base template.
    
    Add an ID (translation ID) to translation link.
    Trigger translate function with correct arguments as a result of user clicking a translate link

--- app/routes.py
+++ app/routes.py
@@ -16,6 +16,7 @@ from app.forms import (
     ResetPasswordForm,
 )
 from app.email import send_password_reset_email
+from app.translate import translate
 from langdetect import detect, LangDetectException
 
 
@@ -249,3 +250,14 @@ def reset_password(token):
         flash(_("Your password has been reset."))
         return redirect(url_for("login"))
     return render_template("reset_password.html", form=form)
+
+
+@app.route("/translate", methods=["POST"])
+@login_required
+def translate_text():
+    data = request.get_json()
+    return {"text": translate(
+        data["text"],
+        data["source_language"],
+        data["dest_language"]
+    )}
